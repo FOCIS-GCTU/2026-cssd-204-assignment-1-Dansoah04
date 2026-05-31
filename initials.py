e4b144b551b7a828ea13040c2f890dff864237b9
Dansoah-04 <2425405019@live.gctu.edu.gh>
2026-05-31 12:43:11 +0000

    Add TDB initials assigment  with proper format

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+#File:initials.py
+#Description: A program that prints my initials in stylized large letters.
+#Assignment Number: Assignment Number, 1
+#
+#Name: Tracy Dansoah Boadu
+#Student ID: 2425405019
+#Email:2425405019@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#
+#On my honor,Tracy Dansoah Boadu,this programming assignment is my own work
+#and I have not provided this code to another student.
 
 def main():
-  pass #code goes here
+# Displaying my initials TDB in 10 lines long and 12 characters wide stylized form.
 
+    print('...TDB')
+
+    print('...TTTTTTTTTTTT........DDDDDDDD............BBBBBBBB.........')
+    print('...TTTTTTTTTTTT........DDDDDDDDDD..........BBBBBBBBBB.......')
+    print('........TT.............DD.......DDD........BB.......BBB.....')
+    print('........TT.............DD........DD........BB.......BBB.....')
+    print('........TT.............DD........DD........BBBBBBBBBB.......')
+    print('........TT.............DD........DD........BBBBBBBBBB.......')
+    print('........TT.............DD........DD........BB.......BBB.....')
+    print('........TT.............DD.......DDD........BB.......BBB.....')
+    print('........TT........**...DDDDDDDDDD.....**...BBBBBBBBBB.....**')
+    print('........TT........**...DDDDDDDD.......**...BBBBBBBB.......**')
 
 
 main()
+
+
+
